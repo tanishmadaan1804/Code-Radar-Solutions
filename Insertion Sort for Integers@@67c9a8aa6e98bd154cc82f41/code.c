8282a3658f02b9ee6b6b5e53db623afb6acc5056
tanishmadaan1804 <tanish1823.becse24@chitkara.edu.in>
2025-04-08 14:56:50 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,19 +1,17 @@
 // Your code here...
-// Your code here...
-int bubbleSort(char arr[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+int insertionSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j = j-1;
         }
+        arr[j+1] = key;
     }
 }
-
-int printArray(char arr[], int n){
+int printArray(int arr[], int n){
     for(int i=0; i<n; i++){
-        printf("%c ", arr[i]);
+        printf("%d ", arr[i]);
     }
 }
