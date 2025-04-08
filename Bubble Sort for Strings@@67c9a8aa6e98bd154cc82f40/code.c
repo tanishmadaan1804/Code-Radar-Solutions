9b1f87032c58fd2e169c53859db9c9ffcebca51c
tanishmadaan1804 <tanish1823.becse24@chitkara.edu.in>
2025-04-08 14:57:28 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,18 +1,15 @@
-// Your code here...
-int bubbleSort(char arr[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+#include <ctype.h>
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    int i = 0;
+    while (message[i] != '\0') {
+        char ch = message[i];
+        if (isalpha(ch)) {
+            char base = islower(ch) ? 'a' : 'A';
+            encrypted[i] = (ch - base + shift) % 26 + base;
+        } else {
+            encrypted[i] = ch;
         }
+        i++;
     }
-}
-
-int printArray(char arr[], int n){
-    for(int i=0; i<n; i++){
-        printf("%c ", arr[i]);
-    }
+    encrypted[i] = '\0'; // Null-terminate the encrypted string
 }
